9ae1a32909d97a8dd35349c7de977528b532d933
LuccaMenezes <luccamen2016@gmail.com>
2023-02-04 11:15:49 -0300

    Rotas com React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
       <h1>Dev Ponto</h1>
+      <Outlet />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Home from './pages/Home';
+
+/*const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])*/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/register",
+        element: <Register />
+      }
+    ]
+  },
+ 
+])
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
